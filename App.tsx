1e757f2fa8c9a83de60d5f500325db7c1dcbfc2c
Andrew Micheal <mykmicheals@gmail.com>
2023-05-17 21:24:20 +0100

    added store provider

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { StoreProvider } from 'easy-peasy';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './src/screens/HomePage';
 import ProductPage from './src/screens/ProductPage';
+import { store } from './src/store/store';
+ 
 
 const Tab = createBottomTabNavigator();
 
 
 export default function App() {
   return (
-  <NavigationContainer>
+    <StoreProvider store={store}>
+
+  
+  <NavigationContainer >
       <Tab.Navigator>
         <Tab.Screen  name="Home" component={HomePage} />
         <Tab.Screen name="Profile" component={ProductPage} />
       </Tab.Navigator>
-    </NavigationContainer>
-  );
+      </NavigationContainer>
+        </StoreProvider>
+      );
+      
 }
 
