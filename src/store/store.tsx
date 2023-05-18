e210e5b53895ae8aeaa7dd7afe77974be52afea8
Andrew Micheal <mykmicheals@gmail.com>
2023-05-18 15:06:50 +0100

    added delete store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -4,7 +4,10 @@ import { createStore,action } from 'easy-peasy';
 export const store = createStore({
     products: [],
   addTodo: action((state:any, payload:any) => {
-    state.products.push({ name: payload.name, price: payload.price, image:payload.image });
+    state.products.push({id:payload.id, name: payload.name, price: payload.price, image:payload.image });
   }),
-  
+  removeProuct: action((state:any, payload:any) => {
+     const index = state.products.findIndex((product:any) => product.id === payload.id);
+      state.products.splice(index, 1);
+  })
 });
