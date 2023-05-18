9518dcbcdace9264e5f9ac0b3667511cdcfcdc8e
Andrew Micheal <mykmicheals@gmail.com>
2023-05-18 20:41:23 +0100

    added deleteModal functionality

---
+++ src/modals/DeleteModal.tsx
@@ -0,0 +1,35 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { styles } from '../styles';
+import ModalContainer from './ModalContainer';
+import { useStoreActions } from 'easy-peasy';
+
+const DeleteModal = ({ showModals, close,productId }: any) => {
+
+    const deleteProuct = useStoreActions((actions: any) => actions.removeProuct);
+    
+    const deleteProductHandler =() => {
+        deleteProuct(productId)
+        close()
+    }
+
+
+  return (
+    <ModalContainer showModals={showModals} onClose={close}>
+      <View style={styles.delModal}>
+        <Text style={styles.delModalText}>Are you sure you want to delete</Text>
+        <View style={styles.modalDelBtm}>
+          <TouchableOpacity onPress={close}>
+            <Text>Cancel</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={deleteProductHandler}>
+            <Text>Yes</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ModalContainer>
+  );
+};
+
+export default DeleteModal;
